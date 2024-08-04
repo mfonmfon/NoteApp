7788dfe70d8777942137310740660ea901420373
mfonmfon <mfonm579@gmail.com>
2024-08-04 03:01:39 +0100

    My note App

--- src/main/java/com/semicolon/africa/mynoteapp/noteController/NoteController.java
+++ src/main/java/com/semicolon/africa/mynoteapp/noteController/NoteController.java
@@ -1,19 +1,18 @@
 package com.semicolon.africa.mynoteapp.noteController;
 
+import com.mongodb.internal.bulk.DeleteRequest;
 import com.semicolon.africa.mynoteapp.dto.Request.AddNoteRequest;
 import com.semicolon.africa.mynoteapp.dto.Request.UpdateNoteRequest;
 import com.semicolon.africa.mynoteapp.dto.Response.AddNoteResponse;
 import com.semicolon.africa.mynoteapp.dto.Response.ApiResponse;
+import com.semicolon.africa.mynoteapp.dto.Response.DeleteNoteResponse;
 import com.semicolon.africa.mynoteapp.dto.Response.UpdateNoteResponse;
 import com.semicolon.africa.mynoteapp.exception.ApiResponseException;
 import com.semicolon.africa.mynoteapp.services.NoteServices;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
@@ -36,13 +35,23 @@ public class NoteController {
     }
 
     @GetMapping
-    public ResponseEntity<?> updatePost(UpdateNoteRequest request){
+    public ResponseEntity<?> updateNote(@RequestBody UpdateNoteRequest request){
         try{
             UpdateNoteResponse response = noteServices.updateNoteWith(request);
             return new ResponseEntity<>(new ApiResponse(true, response), HttpStatus.OK);
         }
         catch(Exception exception){
             return new ResponseEntity<>(new ApiResponse(false,exception), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @DeleteMapping
+    public ResponseEntity<?> deleteNote(@PathVariable("/{id") String id){
+        try{
+            DeleteNoteResponse response = noteServices.deleteNote(id);
+            return new ResponseEntity<>(new ApiResponse(true, response), HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception), HttpStatus.BAD_REQUEST);
         }
 
     }
